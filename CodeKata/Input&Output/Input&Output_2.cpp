7c8e572b0ff6f0eb2c7ec2a51679e06cfcaba866
Mayank Pathak <mayankpathak4513@gmail.com>
2021-10-13 16:25:06 +0530

    Create  Input&Output_2.cpp

---
+++ CodeKata/Input&Output/Input&Output_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,x,y,z,n;
+	cin>>a>>b>>c>>x>>y>>z>>n;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" "<< n;
+	return 0;
+}
