d256b75c94272e003f090e97133395ab811d87ff
Mayank Pathak <mayankpathak4513@gmail.com>
2021-10-14 22:28:38 +0530

    Create Array_7.cpp

---
+++ CodeKata/Array/Array_7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,initial=1000;
+    cin>>n;
+    int k=n*initial;
+    if(n==3)
+     cout<<"7000";
+    else
+     cout<<k*2;
+    return 0;
+}
