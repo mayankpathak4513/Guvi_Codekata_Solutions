4b79b9c20d3e92e32a5314213f9f1abf669d2a86
Mayank Pathak <mayankpathak4513@gmail.com>
2021-10-17 21:31:47 +0530

    Create basics_2.cpp

---
+++ CodeKata/Basics/basics_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,c=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c++;
+    }
+    if(c==2)
+    {
+        cout<<"no";
+    }
+    else
+    {
+        cout<<"yes";
+    }
+    return 0;
+}
