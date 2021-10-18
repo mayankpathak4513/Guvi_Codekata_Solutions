0d23e8a47f1925e347018d738097fefd778ae46c
Mayank Pathak <mayankpathak4513@gmail.com>
2021-10-18 22:42:50 +0530

    Create basics_6.cpp

---
+++ CodeKata/Basics/basics_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,l,r;
+    cin>>n>>l>>r;
+    if(n<r && n>l)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    return 0;
+}
