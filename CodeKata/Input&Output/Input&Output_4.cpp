88ca39d3521ee1ec4ed068baf9b9da950a97cf6f
Mayank Pathak <mayankpathak4513@gmail.com>
2022-01-08 21:25:57 +0530

    Create Input&Output_4.cpp

---
+++ CodeKata/Input&Output/Input&Output_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a,b,c,d,e,f;
+   cin>>a>>b>>c>>d>>e>>f;
+   cout<<a<<" "<<b<<"\n";
+   cout<<c<<" "<<d<<"\n";
+   cout<<e<<" "<<f<<"\n";
+   
+    return 0;
+}
